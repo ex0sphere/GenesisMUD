a3081f8fbd24529c1cbf0b7cbd6ba23371c764d1
ex0sphere <immitis11@gmail.com>
2025-02-25 01:12:48 +0300

    Create Utility - Gag Text.js

---
+++ Triggers and Aliases/Utility - Gag Text.js
@@ -0,0 +1,9 @@
+/*
+AUTHOR: Unknown
+Contains: 1 trigger
+
+Simple gag trigger. Removes lines of text that contain the pattern.
+*/
+
+var mudoutput = document.getElementById('mudoutput');
+mudoutput.removeChild(mudoutput.lastChild)
