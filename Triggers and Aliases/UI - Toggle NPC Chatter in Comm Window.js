36e3133be4976dd7cd8bb91e76f86ac312d586a9
ex0sphere <immitis11@gmail.com>
2025-03-05 11:19:49 +0300

    Update UI - Toggle NPC Chatter in Comm Window.js

--- Triggers and Aliases/UI - Toggle NPC Chatter in Comm Window.js
+++ Triggers and Aliases/UI - Toggle NPC Chatter in Comm Window.js
@@ -6,5 +6,10 @@ Turns NPC chatter appearing in the communication window off or on.
 
 USAGE: comms off/on
 */
-mud.Process.outgoing_gmcp("core.options", {"npc_comms" : args[1]});
-gwc.output.append("NPC comms turned "+args[1]+"!")
+if(args[1] == "off" || args[1] == "on"){
+    mud.Process.outgoing_gmcp("core.options", {"npc_comms" : args[1]});
+    gwc.output.append("NPC comms turned "+args[1]+"!")
+}
+else {
+    gwc.output.append("Usage: <comms off/on>")
+}
