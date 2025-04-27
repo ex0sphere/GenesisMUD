6d82e7472a57780a757bf9bd291e686ec8306f31
ex0sphere <immitis11@gmail.com>
2025-04-27 22:16:32 +0300

    Added more messengers to the pattern

--- Triggers and Aliases/UI - Messengers in Comm Window.js
+++ Triggers and Aliases/UI - Messengers in Comm Window.js
@@ -6,13 +6,13 @@ Puts a timestamped message in the comm window when you receive a message.
 
 Name: UI - Messengers in Comm Window
 Type: regexp
-Pattern: (ferret runs towards you and climbs up to your shoulder|A brisk waif runs up to you|flits up to you|scuttles up to you with a message|monkey runs towards you|skitters to you and gives you a message|runs up to you with a message|flies up to you with a message from|Postmaster tells you|A red-winged harrekki descends from above|lands on your shoulder|scampers up to you|scurries up to you with a message|runs up and sits in your hand|A young page arrives jogging and hands you|A tiny monkey runs towards you and jumps back on your shoulder|grabs onto your leg|A small black snake slithers out of the shadows and coils itself around your arm\.)
+Pattern: (^A ferret scampers in\, climbs up your leg and gives you a message\.$|bat screeches menacingly as it lands on your head|ferret runs towards you and climbs up to your shoulder|A brisk waif runs up to you|flits up to you|scuttles up to you with a message|monkey runs towards you|skitters to you and gives you a message|runs up to you with a message|flies up to you with a message from|Postmaster tells you|A red-winged harrekki descends from above|lands on your shoulder|scampers up to you|scurries up to you with a message|runs up and sits in your hand|A young page arrives jogging and hands you|A tiny monkey runs towards you and jumps back on your shoulder|grabs onto your leg|A small black snake slithers out of the shadows and coils itself around your arm\.)
 
 Execute the following javascript:
 */
 
-  const now = new Date();
-  const hour = String(now.getHours()).padStart(2, '0');
-  const minute = String(now.getMinutes()).padStart(2, '0');
-  const currentTime = `${hour}:${minute}`;
-  document.getElementById('communication').append("["+currentTime+"] You've received a message!\n");
+const now = new Date();
+const hour = String(now.getHours()).padStart(2, '0');
+const minute = String(now.getMinutes()).padStart(2, '0');
+const currentTime = `${hour}:${minute}`;
+document.getElementById('communication').append("["+currentTime+"] You've received a message!\n");
