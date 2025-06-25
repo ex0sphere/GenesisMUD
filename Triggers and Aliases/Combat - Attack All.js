98896bb7a770f2f603f6033a177799d9a7777be6
ex0sphere <immitis11@gmail.com>
2025-06-25 14:50:04 +0300

    Updated "ka" to set enemy type

--- Triggers and Aliases/Combat - Attack All.js
+++ Triggers and Aliases/Combat - Attack All.js
@@ -13,7 +13,9 @@ gwc.userdata.enemy=(args['*'])
 // 2. ALIAS: ka - Count enemies in a room and attack them all
 
 //Pattern: ka
-let enemy = args["*"] || gwc.userdata.enemy
+
+gwc.userdata.enemy = args["*"] || gwc.userdata.enemy;
+let enemy = gwc.userdata.enemy
 
 if(enemy.endsWith("f")){
 	enemy = enemy.replace(/f$/g,"ve") // fix -f enemies (elf, dwarf)
@@ -35,7 +37,33 @@ else {
 	gwc.connection.send("count "+enemy+"s")
 }
 
-// 2.1. Directional aliases for manual use:
+/* ------------------- */
+
+// 3. TRIGGER: Enemy Counter
+// Name: Enemy Counter
+// Type: regexp
+// Pattern: ^(You (count|find) (a single|two|three|four|five|six|seven|eight|nine|ten)|You don&#39;t find any)
+
+if(args[3]!=undefined){
+
+const numbers = { 
+  'a single': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9, 'ten': 10, 'eleven':11, 'twelve':12, 'thirteen':13, 'fourteen':14, 'fifteen':15
+  }
+let howMany = numbers[args[3]]
+const adjectives = {
+	1:'first', 2:'second', 3:'third', 4:'fourth', 5:'fifth', 6:'sixth', 7:'seventh', 8:'eighth',9:'ninth',10:'tenth',11:'eleventh',12:'twelfth',13:'thirteenth',14:'fourteenth',15:'fifteenth'
+}
+
+  for (let i = 1; i <= howMany; i++) {
+    gwc.connection.send("kill "+adjectives[i]+" "+gwc.userdata.enemy)
+  }
+}
+
+gwc.trigger.disable("Enemy Counter")
+
+/* ------------------- */
+
+// 4. OPTIONAL: Directional aliases for manual use:
 
 //Pattern: dk
 gwc.connection.send("d",true);
@@ -76,27 +104,3 @@ gwc.connection.send("ka",true)
 //Pattern: wk
 gwc.connection.send("w",true);
 gwc.connection.send("ka",true)
-
-/* ------------------- */
-
-// 3. TRIGGER: Enemy Counter
-// Name: Enemy Counter
-// Type: regexp
-// Pattern: ^(You (count|find) (a single|two|three|four|five|six|seven|eight|nine|ten)|You don&#39;t find any)
-
-if(args[3]!=undefined){
-
-const numbers = { 
-  'a single': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9, 'ten': 10, 'eleven':11, 'twelve':12, 'thirteen':13, 'fourteen':14, 'fifteen':15
-  }
-let howMany = numbers[args[3]]
-const adjectives = {
-	1:'first', 2:'second', 3:'third', 4:'fourth', 5:'fifth', 6:'sixth', 7:'seventh', 8:'eighth',9:'ninth',10:'tenth',11:'eleventh',12:'twelfth',13:'thirteenth',14:'fourteenth',15:'fifteenth'
-}
-
-  for (let i = 1; i <= howMany; i++) {
-    gwc.connection.send("kill "+adjectives[i]+" "+gwc.userdata.enemy)
-  }
-}
-
-gwc.trigger.disable("Enemy Counter")
