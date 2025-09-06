a63d78f7f6f217b342c647d4abe2a9e771742fe6
ex0sphere <immitis11@gmail.com>
2025-09-06 18:59:04 +0300

    Added echo color and text shadow

---
+++ Triggers and Aliases/UI - Font Changer.js
@@ -0,0 +1,52 @@
+/*
+AUTHOR: Exosphere (big credit to raevynne!!!)
+Contains: 1 alias
+
+Easily change webclient font size and color via CSS attributes.
+
+USAGE EXAMPLES: 
+font color pink    - sets font color to pink
+font color #00ffff - sets font color to #00ffff
+font size 16px     - sets font size to 16px
+font family Comic Sans MS - sets the font to Comic Sans MS
+font echo white    - sets the color of your echoed commands to white
+font shadow #720000 1px 0px 10px - gives the font a fancy red glow
+
+font [color/size/family/echo/shadow] default - resets the attribute to default: #d0d0d0 / 14px / Source Code Pro
+*/
+
+// For reference, background color is #1c1c1c.
+
+let color
+let size
+let family
+let echo
+let shadow
+
+if(args[2] == "default"){
+  color = "#d0d0d0";
+    size  = "14px";
+    family = "Source Code Pro,monospace";
+    echo = "#d68936";
+    shadow = "";
+}
+else {
+  color = args["*"].replace("color","");
+    size  = args["*"].replace("size","");
+    family  = args["*"].replace("family","");
+    echo = args["*"].replace("echo","");
+    shadow = args["*"].replace("shadow","");
+}
+switch(args[1]){
+  case "color":
+    $("#mudoutput").css("color", color);
+    $("#magicmap").css("color", color); break;
+  case "size":
+    $("#mudoutput").css("font-size", size); break;
+  case "family":
+    $("#mudoutput").css("font-family", family); break;
+  case "echo":
+    $(".outgoing").css("color", echo); break;
+  case "shadow":
+    $("#mudoutput").css("text-shadow", shadow); break;
+}
